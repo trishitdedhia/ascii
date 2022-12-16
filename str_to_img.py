1fc6dd99deaeb3e3a2d838db3c342ea035cdcc28
Arnav Rustagi <arnrustagi@gmail.com>
2022-12-16 12:28:24 +0530

    added saving to image, -s,--saves to image, while -o,--output outputs to .txt file

---
+++ str_to_img.py
@@ -0,0 +1,13 @@
+from PIL import Image, ImageDraw, ImageFont
+
+def ascii_to_image (lines : list[str], location : str ,font_width=27, font_height=20, padding=4096):
+    shape=(font_width*80+2*padding, font_height*len(lines)+3*padding)
+    img = Image.new('RGB',shape, color=(0,0,0))
+    drawer=ImageDraw.Draw(img)
+    font=ImageFont.truetype('./assets/sourcecodepro.ttf',24)
+
+    text_loc=(padding,padding)
+    drawer.multiline_text(text_loc,'\n'.join(lines),font=font,fill=(255,255,255))
+    print(f'saving to ./{location}')
+    img.save(location)
+
