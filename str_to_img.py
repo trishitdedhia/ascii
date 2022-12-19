89fc0613a7dc6ebcd2f6ef07b9803ec921bc21e6
Trishit Dedhia <58739067+trishitdedhia@users.noreply.github.com>
2022-12-19 21:55:13 +0530

    Removed default padding

--- str_to_img.py
+++ str_to_img.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 
-def ascii_to_image (lines : list[str], location : str ,font_width=27, font_height=20, padding=4096):
+def ascii_to_image (lines : list[str], location : str ,font_width=27, font_height=20, padding=0):
     shape=(font_width*80+2*padding, font_height*len(lines)+3*padding)
     img = Image.new('RGB',shape, color=(0,0,0))
     drawer=ImageDraw.Draw(img)
